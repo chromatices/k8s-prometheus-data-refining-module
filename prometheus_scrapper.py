3b9f8c5951f2bdf8fcd475639e9e11f76bd30b37
chromatices <pjh403@naver.com>
2022-11-14 09:48:56 +0900

    enable custom_dir method in line 25

--- prometheus_scrapper.py
+++ prometheus_scrapper.py
@@ -22,8 +22,8 @@ def main(url:str, start_time:str, end_time:str, chunk_size:str, storage_path:str
     chunk_size = parse_timedelta(end_time, chunk_size)
     storage_path = storage_path
 
-    # custom_dir(storage_path)
-    # f =  open(storage_path +"/error_variable.txt",'w')
+    custom_dir(storage_path)
+    f =  open(storage_path +"/error_variable.txt",'w')
 
     # import avaliable metric's name
     variable_name = prom.all_metrics()
